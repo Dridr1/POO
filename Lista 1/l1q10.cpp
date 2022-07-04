fb936c1efc44d414c02212f08fa1d3f085f65b27
Dridr1 <a.driano@outlook.com>
2022-07-04 17:59:20 -0300

    atualizado exercício 10

--- Lista 1/l1q10.cpp
+++ Lista 1/l1q10.cpp
@@ -1,60 +1,82 @@
 #include <iostream>
 using namespace std;
 
-void displayResult(float a, float b, float ans, char opt);
 float calculate(float a, float b, char opt);
+bool verifyOperation(char opt);
+// void isItNice(float ans); //Essa função é apenas uma brincadeira interna entre amigos :))
 
 int main()
 {
-    char usrOpt, opt;
-    float a, b;
+    float a, b, ans;
+    char opt, rewind;
+    bool isValid;
 
     do
     {
         cout << "Insira o primeiro número: ";
         cin >> a;
-        cout << "Insira a operação que deseja realizar: ";
+        cout << "Insira a operação desejada [+|-|*|/]: ";
         cin >> opt;
+
+        isValid = verifyOperation(opt);
+        if (isValid == false)
+        {
+            cout << "Operação inválida" << endl;
+            rewind = 's';
+            continue;
+        }
+
         cout << "Insira o segundo número: ";
         cin >> b;
+        ans = calculate(a, b, opt);
+        cout << a << opt << b << "=" << ans << endl;
+        
+        // isItNice(ans);
 
-        float ans = calculate(a, b, opt);
-        displayResult(a, b, ans, opt);
+        while (true)
+        {
+            cout << "Deseja realizar outra operação? [s/N]: ";
+            cin >> rewind;
 
-        cout << "Deseja realizar outra operação? [s/n]: ";
+            if (rewind == 's' || rewind == 'S' || rewind == 'N' || rewind == 'n')
+                break;
+        }
 
-    } while (usrOpt  == 's' || usrOpt == 'S');
-    
-    
+    } while (rewind == 's' || rewind == 'S');
+
+    cout << "Tchau, se cuida!" << endl;
+
+    return 0;
 }
 
 float calculate(float a, float b, char opt)
 {
-    float ans;
-
     switch (opt)
     {
     case '+':
-        ans = a + b;
-        break;
+        return a + b;
     case '-':
-        ans = a - b;
-        break;
+        return a - b;
     case '*':
-        ans = a * b;
-        break;
+        return a * b;
     case '/':
-        ans = a / b;
-        break;
-    default:
-        cout << "Operação inválida";
-        break;
+        return a / b;
     }
-
-    return ans;
+    return 0;
 }
 
-void displayResult(float a, float b, float ans, char opt)
+bool verifyOperation(char opt)
 {
-    cout << a << opt << b << "=" << ans << endl;
+    if (opt == '+' || opt == '-' || opt == '*' || opt == '/')
+        return true;
+    else
+        return false;
 }
+
+// void isItNice(float ans)
+// {
+//     if (ans == 69)
+//         cout << "( ͡° ͜ʖ ͡°) Nice!" << endl;
+//     else if (ans == 420)
+//         cout << "( ͡o ͜ʖ ͡o) ohhh, weed!" << endl;
+// }
