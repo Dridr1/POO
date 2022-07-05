620e65fb9fbb9394db5b66932594539cef7af95c
Dridr1 <a.driano@outlook.com>
2022-07-05 18:41:30 -0300

    adicionada pasta com configurações do vscode no gitignore e adicionada resolução do exercício 7 da lista 1

--- Lista 1/l1q7.cpp
+++ Lista 1/l1q7.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 using namespace std;
 
+
+void mostraEspaco(int n);
+void mostraPadrao(int n);
+
 int main()
 {
-    //Sem saco pra fazer essa porra;
+    int s = 4;
+
+    for (int i = 1; i <= 5; i++)
+    {
+        mostraEspaco(s);
+        s--;
+        mostraPadrao(i);
+    }
+}
+
+void mostraPadrao(int n)
+{
+    for (int j = 1; j <= n; j++)
+    {
+        cout << n << " ";
+    }
+    cout << endl;
+}
+
+void mostraEspaco(int n)
+{
+    for (int k = 0; k < n; k++)
+    {
+        cout << " ";
+    }
+    
 }
